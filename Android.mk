810e404b0b1e825b911931f0f8b924c899646443
Samaelson <35295868+Samaelson@users.noreply.github.com>
2024-02-27 10:03:50 +0100

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -2,16 +2,10 @@ LOCAL_PATH := $(my-dir)
 ################## Start of AOSPHost target ##################
 include $(CLEAR_VARS)
 LOCAL_MODULE := SygicNavigation
-LOCAL_LICENSE_KINDS := legacy_notice
-LOCAL_LICENSE_CONDITIONS := notice
-LOCAL_MODULE_OWNER := google
 LOCAL_SRC_FILES := sygic-truck-gps-navigation.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_BUILT_MODULE_STEM := package.apk
 LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_PRIVILEGED_MODULE := true
-LOCAL_PRODUCT_MODULE := true
-LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_PATH := $(TARGET_OUT_DATA)
 include $(BUILD_PREBUILT)
